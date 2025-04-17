5fbe8fef077e7c3986b5f1ed96cac6e6faf418f3
Nikpop228 <nikipet15.06@gmail.com>
2025-04-18 00:29:57 +0300

    comments added

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream> //подключение библиотек
 #include <string>
 
 int main()
 {
-	std::string name;
-	std::cin >> name;
-	std::cout << "Hello world!";
+	std::string name; // переменная для имени
+	std::cin >> name; // ввод имени
+	std::cout << "Hello world!"; // вывод сообщения
 }

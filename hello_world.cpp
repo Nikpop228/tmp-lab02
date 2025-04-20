78330e59a4cd01bfbacc56e8105e9c919c04ef68
Nikpop228 <nikipet15.06@gmail.com>
2025-04-20 22:16:37 +0300

    changed comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
-#include <iostream> //including libraries
+#include <iostream> //including l,i,b,r,a,r,i,e,s,
 #include <string>
 
 int main()
 {
-	std::string name; // переменна€ дл€ имени
-	std::cin >> name; // ввод имени
+	std::string name; // name
+	std::cin >> name; // 
 	std::cout << "Hello world!"; // вывод сообщени€
 }
